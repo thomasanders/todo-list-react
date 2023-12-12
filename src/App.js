0bb7663879ea487f69772f21b3cb4a239cfefee9
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-12-12 14:54:39 +0200

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,35 +1,84 @@
-import React from "react";
+import { useState, useEffect } from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
-
-const tasks = [
-  { id: 1, content: "Download ToDo Task List", done: true },
-  { id: 2, content: "Eat Pierogi", done: false },
-];
-
-const hideDoneTasks = false
+// import Section from "./Section";
+// import Header from "./Header";
+// import Container from "./Container";
 
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks]);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks => tasks.filter(task => task.id !== id));
+  };
+
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done, };
+      }
+
+      return task;
+    }));
+  };
+
+  const toggleAllDone = () => {
+    setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
+  };
+
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+        content,
+        done: false,
+      },
+    ]);
+  };
+
   return (
-    <main className="main">
-      <h1 className="main__title">Tasks list</h1>
-      <div className="section">
-        <h2 className="section__header">Add new task</h2>
-        <div className="section__main">
-          <Form/>
-        </div>
-      </div>
-      <div className="section">
-        <div className="section__header section__headerSecondary">
-          <h2>Tasks List </h2>
-          <Buttons tasks={tasks}/>
-        </div>
-        <div className="section__main">
-          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks}/>
-        </div>
-      </div>
-    </main>
+    <Container>
+      <Header title="Tasks list" />
+
+      <Section
+        title="Add new task"
+        body={<Form addNewTask={addNewTask} />}
+      />
+
+      <Section
+        title="Tasks list"
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            key={tasks.id}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            toggleAllDone={toggleAllDone}
+          />}
+        body={
+          <Tasks
+            tasks={tasks}
+            key={tasks.id}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />}
+      />
+    </Container>
   );
 }
 

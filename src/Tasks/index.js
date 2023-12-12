916f381552fea014c1ed62b664255bce07761656
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-12-12 13:59:24 +0200

    tasks index.js added

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "./style.css"
 
 const Tasks = (props) => (
   <ul className=" task__list">
     {props.tasks.map((task) => (
-      <li>{task.content}</li>
+      <li
+       className={`task__list ${task.done && props.hideDoneTasks ? " task__list--hidden" : ""}`}><button className="task__js--done">
+       {task.done ? "✔" : ""}
+       </button>
+       <span className={`task__content ${task.done ? "task__content--done" : ""}`}>
+       {task.content}
+       </span>
+       <button className="task__js--remove">
+        🗑
+       </button>
+       </li>
     ))}
   </ul>
 );

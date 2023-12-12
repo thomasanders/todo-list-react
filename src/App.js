4471953503d7600657175e58c34ca8c8643d4aaf
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-12-12 15:14:47 +0200

    screen added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { useState, useEffect } from 'react';
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
-// import Section from "./Section";
-// import Header from "./Header";
-// import Container from "./Container";
+import Section from "./Section";
+import Header from "./Header";
+import Container from "./Container";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);

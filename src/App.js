f932bacfebe62161aa1e9e3c5467f4ec693bd68d
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-12-12 12:28:55 +0200

    html added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from "./logo.svg";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+  <main className="main">
+  <h1 className="main__title">Tasks list</h1>
+  <div className="section">
+      <h2 className="section__header">Add new task</h2>
+      <div className="section__main">
+          <form className="js-form form">
+              <input className="js-newTask form__newTask" placeholder="add new task" name="To do" type="text"
+                  autocomplete="off"/>
+              <button className="form__button">Add new task</button>
+          </form>
+      </div>
+  </div>
+  <div className="section">
+      <div className="section__header section__headerSecondary">
+          <h2>Tasks List </h2>
+          <div className="task__newButtons js-newButtons"></div>
+      </div>
+      <div className="section__main">
+          <ul className="js-tasks task__list"></ul>
+      </div>
+  </div>
+</main>);
 }
 
 export default App;

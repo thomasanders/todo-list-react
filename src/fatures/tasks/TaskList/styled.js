8fce7906efa300ffd4744b17806989c4565c5b4e
Andrzej <andrzej.krzysztof.hryniewicz@gmail.com>
2024-02-02 11:53:24 +0200

    cosmetics

--- src/fatures/tasks/TaskList/styled.js
+++ src/fatures/tasks/TaskList/styled.js
@@ -1,58 +1,64 @@
 import styled, { css } from "styled-components";
 
 export const List = styled.ul`
-    padding: 30px 10px;
-    margin: auto;
-    list-style: none;
+  padding: 30px 10px;
+  margin: auto;
+  list-style: none;
 `;
 
 export const Item = styled.li`
-    padding: 10px;
-    display: grid;
-    grid-template-columns: auto 1fr auto;
-    gap: 10px;
-    align-items: center;
-    border-top: 1px solid #eee;
+  padding: 10px;
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  gap: 10px;
+  align-items: center;
+  border-top: 1px solid #eee;
 
-    ${({ hidden }) => hidden && css`
-        display: none;
+  ${({ hidden }) =>
+    hidden &&
+    css`
+      display: none;
     `}
 `;
 
 export const Content = styled.span`
-    ${({ done }) => done && css`
-        text-decoration: line-through;
+  ${({ done }) =>
+    done &&
+    css`
+      text-decoration: line-through;
     `}
 `;
 
 export const StyledButton = styled.button`
-    color: white;
-    width: 30px;
-    height: 30px;
-    padding: 0;
-    border: none;
-    transition: 0.5s;
-    background-color: teal;
-    cursor: pointer;
-    
-    
-;
+  color: white;
+  width: 30px;
+  height: 30px;
+  padding: 0;
+  border: none;
+  background-color: teal;
+  transition: 0.5s;
+  cursor: pointer;
 
-    /* ${({ Done }) => Done && css`
-        background-color: green;
 
-        &:hover {
-            background-color: rgb(55, 161, 23);
-            cursor: pointer;
-        }
-    `} */
+  ${({ done }) =>
+    done &&
+    css`
+      background-color: teal;
 
-    ${({ remove }) => remove && css`
-        background-color: red;
+      &:hover {
+        background-color: rgb(55, 161, 23);
+        cursor: pointer;
+      }
+    `}
 
-        &:hover {
-            background-color: rgb(235, 26, 61);
-            cursor: pointer;
-        }
+    ${({ remove }) =>
+    remove &&
+    css`
+      background-color: red;
+
+      &:hover {
+        background-color: rgb(235, 26, 61);
+        cursor: pointer;
+      }
     `}
 `;

ce2f18fdc169f5e2989ff8391fd5d774df4f22e7
Andrzej <andrzej.krzysztof.hryniewicz@gmail.com>
2024-02-03 11:59:34 +0200

    AuthorPage renamed

---
+++ src/fatures/author/Author.js
@@ -0,0 +1,86 @@
+import React from "react";
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+// eslint-disable-next-line
+export default () => (
+  <Container>
+    <Header title="Creator" />
+    <Section
+      title="Andrzej Hryniewicz"
+      body={
+        <>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non
+          libero augue. Morbi metus justo, lacinia ut tincidunt tincidunt,
+          iaculis vel est. Nam porttitor bibendum massa, ut convallis orci
+          consequat et. Mauris a ex felis. Proin eu eros eget dui ornare
+          aliquet. Morbi arcu ligula, laoreet ut accumsan eget, semper in
+          tellus. Curabitur ornare eros at congue condimentum. Vestibulum
+          vehicula ac orci nec fringilla. Phasellus elementum libero a ligula
+          consectetur, sed viverra magna volutpat. Nam tempus, metus eget
+          dignissim aliquam, enim arcu mollis quam, nec ultrices urna enim non
+          ipsum. Mauris dapibus et diam at pretium. Duis arcu velit, convallis
+          sit amet risus vitae, vulputate ultricies nunc. Sed non nibh tortor.
+          Etiam commodo velit et mi faucibus laoreet. Vestibulum vel hendrerit
+          leo, pharetra fringilla felis. Aenean elementum ligula ut metus
+          ornare, et suscipit turpis porttitor. Nunc mollis efficitur metus a
+          ultrices. Praesent diam risus, feugiat vitae odio sit amet, elementum
+          elementum purus. Phasellus pulvinar tortor sed lacinia hendrerit.
+          Etiam est sapien, porta a turpis a, semper auctor tellus. Nunc purus
+          erat, varius aliquam luctus non, cursus ut lorem. Donec non purus quis
+          orci consequat vehicula. Interdum et malesuada fames ac ante ipsum
+          primis in faucibus. In placerat egestas lacus, at faucibus nisi
+          tincidunt in. Donec mi leo, lacinia eget convallis ut, tincidunt at
+          ligula. Suspendisse mattis varius dolor, ut scelerisque eros consequat
+          a. Praesent tincidunt fringilla mi, sit amet vehicula ante dignissim
+          sit amet. Maecenas gravida efficitur sapien, a aliquet quam elementum
+          et. Praesent eget imperdiet sem. Morbi semper rhoncus dolor, in congue
+          leo ultricies in. Praesent et luctus dolor. Nunc posuere, felis sed
+          tincidunt posuere, odio lorem aliquet justo, sit amet condimentum
+          velit libero molestie eros. Integer non turpis feugiat nisi
+          consectetur cursus sit amet vel sem. Praesent viverra mattis mi a
+          vulputate. In bibendum ante nec fermentum sollicitudin. Fusce
+          ultricies accumsan dui, nec suscipit ante ullamcorper id. Praesent
+          tincidunt lectus velit, nec accumsan odio faucibus non. Fusce
+          consequat augue sed malesuada tempus. Aenean auctor sed arcu at
+          aliquam. Nullam rhoncus ex nec lacus condimentum sagittis. Nam non
+          ligula auctor, consectetur lorem ac, luctus leo. Suspendisse
+          fringilla, nibh ac placerat bibendum, tortor tortor hendrerit libero,
+          a facilisis velit mauris eu urna. Sed scelerisque augue massa, non
+          lobortis tellus venenatis hendrerit. Nullam volutpat bibendum egestas.
+          Sed ut turpis dolor. Nam imperdiet suscipit sodales. Quisque vulputate
+          erat ac scelerisque fermentum. Nulla vehicula urna ligula, non euismod
+          magna aliquet iaculis. Nullam quis elit sit amet mauris luctus blandit
+          quis vitae tellus. Suspendisse auctor luctus eros, vel fermentum
+          mauris sollicitudin vitae. Nam vulputate metus fringilla pulvinar
+          cursus. Maecenas urna elit, tempor vel ipsum et, tristique malesuada
+          neque. Sed at blandit tortor, sed blandit risus. In elementum urna ac
+          felis dictum mollis. Interdum et malesuada fames ac ante ipsum primis
+          in faucibus. Integer pretium auctor ligula. Donec gravida non justo ut
+          aliquam. Proin viverra sollicitudin rhoncus. Proin non orci vitae sem
+          mollis vehicula. Vestibulum ante ipsum primis in faucibus orci luctus
+          et ultrices posuere cubilia curae; Sed vitae metus pulvinar, tincidunt
+          dui vel, efficitur lectus. Pellentesque quis sagittis eros, a
+          tristique est. Donec vitae tellus ipsum. Donec vulputate bibendum
+          lorem ut efficitur. Donec placerat turpis id tortor efficitur,
+          tristique condimentum velit ornare. Nam interdum leo et urna tincidunt
+          ultricies. Ut maximus sit amet nisi non semper. Nunc suscipit magna ac
+          placerat sollicitudin. Nulla vehicula orci sagittis ex ultrices, at
+          pretium ipsum egestas. Duis vitae libero nec turpis egestas vestibulum
+          imperdiet at odio. Vestibulum ultricies dignissim placerat. Vestibulum
+          ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; Pellentesque habitant morbi tristique senectus et netus et
+          malesuada fames ac turpis egestas. Donec vitae finibus ex. Sed vitae
+          pharetra odio, a molestie turpis. Maecenas ut pellentesque dolor. Orci
+          varius natoque penatibus et magnis dis parturient montes, nascetur
+          ridiculus mus. Donec mollis lacus nisi, id pulvinar justo suscipit
+          quis. Nullam ultricies erat a justo scelerisque euismod. Quisque risus
+          quam, feugiat non ante quis, pretium convallis ex. Nam rhoncus
+          hendrerit arcu, ac congue velit ullamcorper et. Morbi gravida erat et
+          interdum finibus.
+        </>
+      }
+    />
+  </Container>
+);

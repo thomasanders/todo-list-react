6021802773fe689d690f902d019a7f24a60c8264
Andrzej <andrzej.krzysztof.hryniewicz@gmail.com>
2024-02-03 12:56:50 +0200

    state

--- src/fatures/tasks/TasksPage/FetchExampleTasksButton/index.js
+++ src/fatures/tasks/TasksPage/FetchExampleTasksButton/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { UseDispatch, useDispatch, useSelector } from "react-redux";
+import {useDispatch, useSelector } from "react-redux";
 import { fetchExampleTasks, selectLoading } from "../../tasksSlice";
 import Button from "../Button";
 
--- src/fatures/tasks/TasksPage/TaskList/index.js
+++ src/fatures/tasks/TasksPage/TaskList/index.js
@@ -9,29 +9,24 @@ import {
 } from "../../tasksSlice";
 import { toTask } from "../../../../routes";
 import searchQueryParamName from "../Search/searchQueryParamName";
-import { useQueryParameter} from "../queryParameters"
+import { useQueryParameter } from "../queryParameters";
 
 const TaskList = () => {
   const query = useQueryParameter(searchQueryParamName);
 
-  const tasks = useSelector(state=> selectTasksByQuery(state, query));
+  const tasks = useSelector((state) => selectTasksByQuery(state, query));
   const hideDone = useSelector(selectHideDone);
 
   const dispatch = useDispatch();
   return (
     <List>
       {tasks.map(task => (
-        <Item
-         key={task.id} 
-         hidden={task.done && hideDone}>
+        <Item key={task.id} hidden={task.done && hideDone}>
           <Button toggleDone onClick={() => dispatch(toggleTaskDone(task.id))}>
             {task.done ? "✔" : ""}
           </Button>
           <Content done={task.done}>
-            <StyledLink to={toTask({ id: task.id })}>
-              
-              {task.content}
-            </StyledLink>
+            <StyledLink to={toTask({ id: task.id })}>{task.content}</StyledLink>
           </Content>
           <Button remove onClick={() => dispatch(removeTask(task.id))}>
             🗑
